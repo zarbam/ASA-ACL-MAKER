82302c91e78fc1bf01b8bc5e4a584b68c1ca9774
zarbam <Zarbam@gmail.com>
2019-04-12 09:12:10 -0400

    Create ACLMaker.sh

---
+++ ACLMaker.sh
@@ -0,0 +1,38 @@
+
+#!/bin/bash
+# Program name: ASA Object Maker
+clear
+# Display Title
+cat << "EOF"
+ ______     ______     __            __    __     ______     __  __     ______     ______
+/\  __ \   /\  ___\   /\ \          /\ "-./  \   /\  __ \   /\ \/ /    /\  ___\   /\  == \
+\ \  __ \  \ \ \____  \ \ \____     \ \ \-./\ \  \ \  __ \  \ \  _"-.  \ \  __\   \ \  __<
+ \ \_\ \_\  \ \_____\  \ \_____\     \ \_\ \ \_\  \ \_\ \_\  \ \_\ \_\  \ \_____\  \ \_\ \_\
+  \/_/\/_/   \/_____/   \/_____/      \/_/  \/_/   \/_/\/_/   \/_/\/_/   \/_____/   \/_/ /_/
+
+
+Created by:MZ
+EOF
+#sleep 2
+clear
+echo "############################### COPY HOSTS BELOW ###############################"
+#  read ip_list.txt
+cat ip_list.txt |  while read output
+do
+        IP="$(ipcalc -n $output | awk '{ if ($1 == "Address:") { print $2 }}')"
+        NM="$(ipcalc -n $output | awk '{ if ($1 == "Netmask:") { print $2 }}')"
+        echo object network obj-$IP
+        echo subnet $IP $NM
+        echo description 
+        echo ex
+done
+echo "############################### END HOSTS ###############################"
+echo "############################### COPY BELOW FOR GROUP ###############################"
+cat ip_list.txt |  while read output
+do
+        IP="$(ipcalc -n $output | awk '{ if ($1 == "Address:") { print $2 }}')"
+        echo object network obj-$IP
+done
+echo "############################### END GROUP ###############################"
+
+
